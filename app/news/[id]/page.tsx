c8cadb3514c4b947625cb71a3a84eaf9ebe45eaa
expressnepal <expressnepaloffice@gmail.com>
2026-09-04 19:21:54 +0545

    Update page.tsx

--- app/news/[id]/page.tsx
+++ app/news/[id]/page.tsx
@@ -408,7 +408,7 @@ export default async function NewsSlugPage({
                       </h2>
                     </div>
 
-                    {/* Cards grid */}
+                    {/* Cards grids */}
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-6">
                       {relatedPosts.map((item) => {
                         const contentImages = extractImagesFromContent(
